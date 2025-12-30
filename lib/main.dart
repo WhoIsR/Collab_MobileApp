b593ba2a62316cfec2ae3fa9d2078c119fc73195
radja <radjasatrio70@gmail.com>
2025-12-30 18:16:15 +0700

    inisialisasi Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:collab_mobile_app/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:collab_mobile_app/data/services/notification_service.dart';
+import 'package:collab_mobile_app/core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  try {
+    // Inisialisasi Firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    debugPrint('✓ Firebase initialized');
+  } catch (e) {
+    debugPrint('Firebase init error: $e');
+  }
+
+
   runApp(const MyApp());
 }
 
@@ -14,13 +28,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Collab App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      // Kita mulai dari Splash Screen cuy
-      // home: const SplashScreen(),
+      title: 'PawsRes',
+      theme: AppTheme.cartoonTheme,
+      home: , //Todo han panggil splash screen lu di sini
+      debugShowCheckedModeBanner: false,
     );
   }
 }
