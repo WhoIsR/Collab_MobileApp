6687add1f6e0ac3a4d8211670bdee05c686ce118
muhamadfarhannabawi <1123150170@global.ac.id>
2025-12-30 21:13:30 +0700

    awal

--- lib/presentation/pages/home/explore_page.dart
+++ lib/presentation/pages/home/explore_page.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:uas_test/core/theme/app_colors.dart';
+import 'package:uas_test/data/models/animal_report_model.dart';
+import 'package:uas_test/presentation/pages/home/detail_page.dart';
 
+class ExplorePage extends StatefulWidget {
+  final List<AnimalReport> reports;
+
+  const ExplorePage({super.key, this.reports = const []});
