d0f1d5141992a8997d5607d3029b54a9f88c2563
muhamadfarhannabawi <1123150170@global.ac.id>
2025-12-30 17:41:28 +0700

    buat timer animasi fix banget banget

--- lib/presentation/pages/splashscreen/splash_farhan.dart
+++ lib/presentation/pages/splashscreen/splash_farhan.dart
@@ -14,7 +14,7 @@ class _Splash1PageState extends State<Splash1Page> {
   void initState() {
     super.initState();
     // Tampil 4 detik
-    Timer(const Duration(seconds: 5), () {
+    Timer(const Duration(seconds: 4), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
