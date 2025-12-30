4c62a8add633a27b846c70901587501131c783a5
yaseumine <yasminmaharani123@gmail.com>
2025-12-30 15:35:45 +0700

    mendefinisikan data diri

---
+++ lib/presentation/pages/profiles/profile-yasmin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfileYasmin extends StatelessWidget {
+  const ProfileYasmin({super.key});
+
+  //data diri
+  final String myName = "Aulia Yasmin Maharani";
+  final String myRole =
+      "Mobile Application Developer"; // Role terdengar lebih pro
+  final String myNim = "1123150146";
+  final String myDescription =
+      "Mahasiswa IT yang berfokus pada pengembangan aplikasi mobile menggunakan Flutter. "
+      "Memiliki ketertarikan mendalam pada UI/UX Design dan Backend Integration. "
+      "Saat ini sedang mengembangkan aplikasi manajemen stok dan sistem POS.";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
