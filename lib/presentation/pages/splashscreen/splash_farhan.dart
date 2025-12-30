445d4ed536bd6e1d6961e5f770ff7fc0b81f6ecb
muhamadfarhannabawi <1123150170@global.ac.id>
2025-12-30 16:39:09 +0700

    buat tengah

--- lib/presentation/pages/splashscreen/splash_farhan.dart
+++ lib/presentation/pages/splashscreen/splash_farhan.dart
@@ -4,3 +4,26 @@ import 'package:tgl_31/screen/profil.dart';
 
 class SplashScreen1 extends StatelessWidget {
   const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+      child:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFFF4F2F7),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('android/assets/images/image_1.jpg'),
+                ),
+              ),
+            ),
