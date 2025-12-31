5515a2bb29cfe1c5fcdef2d8a8776d30a244230c
radja <radjasatrio70@gmail.com>
2025-12-31 09:46:23 +0700

    membuat komponen atau widget untuk foto di bagian header detail page

--- lib/presentation/pages/home/detail_page.dart
+++ lib/presentation/pages/home/detail_page.dart
@@ -13,9 +13,36 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.secondary,
       appBar: AppBar(title: const Text('Detail Laporan')),
-      body: const Center(
-        child: CircularProgressIndicator(),
-      ), // Placeholder dulu
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
+class _HeaderImage extends StatelessWidget {
+  final String imageUrl;
+
+  const _HeaderImage({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(27)),
+      child: SizedBox(
+        width: double.infinity,
+        height: 300,
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+          errorBuilder: (ctx, error, stack) => Container(
+            color: Colors.grey[200],
+            child: const Icon(
+              Icons.pets,
+              size: 100,
+              color: AppColors.textOutline,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
