ed72db944cc2460390670ae4f1edad1a3cca41f4
yaseumine <yasminmaharani123@gmail.com>
2026-01-01 12:53:37 +0700

    membuat state untuk proses loading

--- lib/presentation/pages/auth/register_page.dart
+++ lib/presentation/pages/auth/register_page.dart
@@ -13,4 +13,14 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
+  bool _isLoading = false;
+  final AuthService _authService = AuthService();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 }
