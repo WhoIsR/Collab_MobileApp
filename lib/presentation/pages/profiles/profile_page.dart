397a10ac6e371f2b49895160da4811d9efa827e1
yaseumine <yasminmaharani123@gmail.com>
2026-01-03 22:06:00 +0700

    membuat class profile page

--- lib/presentation/pages/profiles/profile_page.dart
+++ lib/presentation/pages/profiles/profile_page.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
