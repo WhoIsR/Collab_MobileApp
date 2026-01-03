a2d6bf7a9cf41d7471077d9ee5030b3129399c7a
yaseumine <yasminmaharani123@gmail.com>
2026-01-03 22:27:56 +0700

    membuat class team member dan mmembuat file profile team page

---
+++ lib/presentation/pages/profiles/profile_team_page.dart
@@ -0,0 +1,15 @@
+class TeamMember {
+  final String name;
+  final String nim;
+  final String photoUrl;
+  final String className;
+  final String skills;
+
+  TeamMember({
+    required this.name,
+    required this.nim,
+    required this.photoUrl,
+    required this.className,
+    required this.skills,
+  });
+}
