05808c734f3c966c2e2bbef2e8843aaf8aa89f86
radja <radjasatrio70@gmail.com>
2026-01-06 15:48:29 +0700

    memperbaiki nama class

--- lib/presentation/pages/profiles/profile_radja.dart
+++ lib/presentation/pages/profiles/profile_radja.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ProfileRadja extends StatelessWidget {
+  const ProfileRadja({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
