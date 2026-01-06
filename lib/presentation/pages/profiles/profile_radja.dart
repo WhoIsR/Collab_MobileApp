0157fa65dc749547438a54ed0f3a4dd223e5916c
radja <radjasatrio70@gmail.com>
2026-01-06 16:21:42 +0700

    menambahkan button social media

--- lib/presentation/pages/profiles/profile_radja.dart
+++ lib/presentation/pages/profiles/profile_radja.dart
@@ -360,6 +360,8 @@ class _ProfilePageState extends State<ProfilePage> {
                   ],
                 ),
               ),
+
+              const SizedBox(height: 24),
             ],
           ),
         ),
