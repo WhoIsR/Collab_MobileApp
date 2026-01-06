1c435595e5bfda8473a719e2b5544e5209a22b22
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-06 14:14:07 +0700

    benerin error

--- lib/presentation/pages/home/my_activity_page.dart
+++ lib/presentation/pages/home/my_activity_page.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:uas_test/core/theme/app_colors.dart';
-import 'package:uas_test/data/models/animal_report_model.dart';
-import 'package:uas_test/data/services/report_service.dart';
-import 'package:uas_test/presentation/pages/home/detail_page.dart';
+import 'package:collab_mobile_app/core/theme/app_colors.dart';
+import 'package:collab_mobile_app/data/models/animal_report_model.dart';
+import 'package:collab_mobile_app/data/services/auth_service.dart';
+import 'package:collab_mobile_app/data/services/report_service.dart';
 
 class MyActivityPage extends StatefulWidget {
   const MyActivityPage({super.key});
