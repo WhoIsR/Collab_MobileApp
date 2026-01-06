e846375862236a7bdd07b1382e0d9ef2ac6558e1
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-06 14:55:06 +0700

    buat import dan oviride

--- lib/presentation/pages/profiles/profile-farhan.dart
+++ lib/presentation/pages/profiles/profile-farhan.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profil Saya'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
