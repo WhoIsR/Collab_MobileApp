f03657f74ac469bf363773e05aeb8f99e81e4a41
radja <radjasatrio70@gmail.com>
2026-01-06 16:04:11 +0700

    setup scaffold dan state variabel

--- lib/presentation/pages/profiles/profile_radja.dart
+++ lib/presentation/pages/profiles/profile_radja.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class ProfileRadja extends StatelessWidget {
-  const ProfileRadja({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isLinkedinHovered = false;
+  bool _isInstagramHovered = false;
+  bool _isGithubHovered = false;
+  bool _isEmailHovered = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E1E1E),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(children: const []),
+        ),
+      ),
+    );
   }
 }
